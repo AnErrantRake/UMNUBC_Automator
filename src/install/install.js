0999f0e3d613d95ba4bb37c33b1cbb7ca6993970
AnErrantRake <stephenanderspeterson@gmail.com>
2019-04-19 11:09:24 -0500

    testing

--- src/install/install.js
+++ src/install/install.js
@@ -13,27 +13,27 @@ var Install = function(){
     PropertiesService.getUserProperties().deleteAllProperties();
 
     // disconnect forms
-    var form = container.getFormUrl();
+    var form = this.container.getFormUrl();
     if(form != null){
       form.removeDestination();
     }
 
     // wipe sheet
-    var sheetList = container.getSheets();
-    this.dashboard = container.insertSheet();
+    var sheetList = this.container.getSheets();
+    this.dashboard = this.container.insertSheet();
     for(var i = 0; i < sheetList.length; i++){
-      container.deleteSheet(sheetList[i]);
+      this.container.deleteSheet(sheetList[i]);
     }
-    dashboard.setName('Dashboard');
+    this.dashboard.setName('Dashboard');
     //remove all cells except 1
-    dashboard.deleteColumns(1, dashboard.getMaxColumns()-1);
-    dashboard.deleteRows(1, dashboard.getMaxRows()-1);
+    this.dashboard.deleteColumns(1, this.dashboard.getMaxColumns()-1);
+    this.dashboard.deleteRows(1, this.dashboard.getMaxRows()-1);
 
     // remove menu
-    container.removeMenu('install');
+    this.container.removeMenu('install');
 
     // remove old triggers
-    var oldTriggers = ScriptApp.getUserTriggers(container);
+    var oldTriggers = ScriptApp.getUserTriggers(this.container);
     for(var i = 0; i < oldTriggers.length; i++){
       ScriptApp.deleteTrigger(oldTriggers[i]);
     }
@@ -43,38 +43,38 @@ var Install = function(){
 
   // builds all fields
   this.pave = function(){
-    // add properties
-
-    // add dashboard
-    install_buildDashboard(container.getActiveSheet());
-    dashboard.insertColumnsAfter(1,2);
-    dashboard.setColumnWidths(1,3,200);
-    //add menu notifier block rows
-    dashboard.insertRowsAfter(1,propServ.getProperty('INSTALL_NOTIFIER_LENGTH')-1);
-    //format menu notifier cell
-    var infoCell = dashboard.getRange('A1:A' + Math.floor(propServ.getProperty('INSTALL_NOTIFIER_LENGTH')));
-    infoCell.merge();
-
-    // add menus
-
-
-    PropertiesService.getDocumentProperties().setProperty('is_installed', true);
-
+    // // add properties
+    //
+    // // add dashboard
+    // install_buildDashboard(this.container.getActiveSheet());
+    // this.dashboard.insertColumnsAfter(1,2);
+    // this.dashboard.setColumnWidths(1,3,200);
+    // //add menu notifier block rows
+    // this.dashboard.insertRowsAfter(1,propServ.getProperty('INSTALL_NOTIFIER_LENGTH')-1);
+    // //format menu notifier cell
+    // var infoCell = this.dashboard.getRange('A1:A' + Math.floor(propServ.getProperty('INSTALL_NOTIFIER_LENGTH')));
+    // infoCell.merge();
+    //
+    // // add menus
+    //
+    //
+    // PropertiesService.getDocumentProperties().setProperty('is_installed', true);
+    //
     return this;
   }
 
   // fills all fields
   this.init = function(){
-    configState();
-    configSecrets();
-
-    genLoaner();
-    // add triggers
-    //add new triggers
-    ScriptApp.newTrigger('dashboard')
-      .forSpreadsheet(container)
-      .onOpen()
-      .create();
+    // configState();
+    // configSecrets();
+    //
+    // genLoaner();
+    // // add triggers
+    // //add new triggers
+    // ScriptApp.newTrigger('dashboard')
+    //   .forSpreadsheet(this.container)
+    //   .onOpen()
+    //   .create();
 
     return this;
   }
